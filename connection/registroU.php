cfd39d8ab30f9e7a69f4117780aac133068ff55a
celestecruzzg <celesteglezcruz@gmail.com>
2024-07-28 23:34:21 -0600

    update

--- connection/registroU.php
+++ connection/registroU.php
@@ -7,17 +7,13 @@ $nombre = $_POST['nombre'];
 $ap_paterno = $_POST['ap_paterno'];
 $ap_materno = $_POST['ap_materno'];
 $curp = $_POST['curp'];
-$sexo = $_POST['sexo'];
+$fecha_na = $_POST['fecha_na'];
 $num_celular = $_POST['num_celular'];
+$sexo = $_POST['sexo'];
 
-
-$query = "INSERT INTO clientes(nombre,ap_paterno,ap_materno,curp,sexo,num_celular) 
-VALUE ('$nombre','$ap_paterno','$ap_materno','$curp','$sexo','$num_celular')";
+$query = "INSERT INTO clientes(nombre, ap_paterno, ap_materno, curp, fecha_na, num_celular, sexo) 
+VALUE ('$nombre','$ap_paterno','$ap_materno','$curp','$fecha_na','$num_celular','$sexo')";
 
 $insert = $conexion->query($query);
 
 header('Location: ./views/register/register.html');
-?>
-
-
-
